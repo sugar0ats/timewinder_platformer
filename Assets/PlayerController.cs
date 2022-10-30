d8e510585d2d1fa185f110bec0dbcab7e6664160
emma <emmacao1234@gmail.com>
2022-10-30 15:19:24 -0700

    flip, left and right run
    and maybe some better movement???

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Flip();
         Run();
         
     }
@@ -28,5 +29,22 @@ public class PlayerController : MonoBehaviour
         myRigidBody.velocity = playerVel;
         bool hasXVel = Mathf.Abs(myRigidBody.velocity.x) > 5;
         myAnimator.SetBool("Run", hasXVel);
+    }
+
+    void Flip()
+    {
+        bool hasXVel = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (hasXVel)
+        {
+            if (myRigidBody.velocity.x > 0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            } 
+
+            if (myRigidBody.velocity.x < -0.1f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
     }
 }
