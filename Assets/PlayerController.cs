c5a488a1c5df15e3fdcdec52495507b512b83aa0
emma <emmacao1234@gmail.com>
2022-11-27 12:21:56 -0800

    Revert "jump!!!"
    This reverts commit 067a54333542a8fa9c863cabbf240dc0f61df91a.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float jumpSpeed;
     public float runSpeed;
     private Rigidbody2D myRigidBody;
     private Animator myAnimator;
-    private BoxCollider2D myFeet;
-    private bool onGround;
-    
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myFeet = GetComponent<BoxCollider2D>(); // get the player's feet. duh.
     }
 
     // Update is called once per frame
@@ -24,8 +19,7 @@ public class PlayerController : MonoBehaviour
     {
         Flip();
         Run();
-        Jump();
-        CheckGrounded();
+        
     }
 
     void Run()
@@ -52,19 +46,5 @@ public class PlayerController : MonoBehaviour
                 transform.localRotation = Quaternion.Euler(0, 180, 0);
             }
         }
-    }
-
-    void Jump()
-    {
-        if (onGround && Input.GetKeyDown(KeyCode.Space))
-        {
-            Vector2 jumpVel = new Vector2(0.0f, jumpSpeed);
-            myRigidBody.velocity = Vector2.up * jumpVel;
-        }
-    }
-
-    void CheckGrounded()
-    {
-        onGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
     }
 }
